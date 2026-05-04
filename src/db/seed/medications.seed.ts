8831748f8eb0da55bbd7b0ea42c01aecdd57b675
Raja Rajeswari <rajeesaravanan160801@gmail.com>
2026-05-04 16:03:51 +0530

    file name updated

--- src/db/seed/medications.seed.ts
+++ src/db/seed/medications.seed.ts
@@ -1,6 +1,6 @@
 import { db } from "../index";
 import { medicationCatalog } from "../schema/medication.schema";
-import { medicationData } from "./medication-data";
+import { medicationData } from "./medication-Catalog";
 
 import { count } from "drizzle-orm";
 
